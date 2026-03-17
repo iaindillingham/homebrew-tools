bb5d33993ca94845663fc9f764a979737d09a1b5
Iain Dillingham <iain@dillingham.me.uk>
2026-03-17 09:27:29 +0000

    Fix license

--- Formula/phantomjs.rb
+++ Formula/phantomjs.rb
@@ -3,7 +3,7 @@ class Phantomjs < Formula
   homepage "https://phantomjs.org/"
   url "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-macosx.zip"
   sha256 "538cf488219ab27e309eafc629e2bcee9976990fe90b1ec334f541779150f8c1"
-  license "BSD"
+  license "BSD-3-Clause"
 
   def install
     bin.install "bin/phantomjs"
