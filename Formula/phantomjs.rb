f82d6b10455ed635b556b4e9d6124e459f6442ba
Iain Dillingham <iain@dillingham.me.uk>
2026-03-17 09:27:03 +0000

    Fix description

--- Formula/phantomjs.rb
+++ Formula/phantomjs.rb
@@ -1,5 +1,5 @@
 class Phantomjs < Formula
-  desc "PhantomJS"
+  desc "Scriptable headless browser"
   homepage "https://phantomjs.org/"
   url "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-macosx.zip"
   sha256 "538cf488219ab27e309eafc629e2bcee9976990fe90b1ec334f541779150f8c1"
